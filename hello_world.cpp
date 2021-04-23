1e79fd4a72231a29f5e0148f415b35dcdca178e4
Brian Musembi <brian.musembi@mths.ca>
2021-04-23 13:41:44 +0000

    added hello_world.cpp

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2021 Brian Musembi All Rights Reserved
+//
+// Created by: Brian Musembi
+// Created on: 23 April 2021
+// This is the "Hello, World!" program, with proper style
+
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello, World!" << std::endl;}
